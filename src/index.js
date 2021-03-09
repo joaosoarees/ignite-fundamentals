d473b9e14539e8e748611836b379a66b475972da
João Vitor <jsoareez@gmail.com>
2021-03-08 23:09:49 -0300

    create first route

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  return res.json({ message: 'Olá' });
+})
+
 app.listen(3003, () => console.log('🔥 Server started at http://localhost:3003/'));
